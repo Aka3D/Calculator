87cb55624834be2fc271e0f38951cd6da639ed52
Akarshan Gnaneswaran <75044525+Aka3D@users.noreply.github.com>
2021-11-18 09:26:55 +0100

    DB

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -25,53 +25,6 @@ namespace Calculator
             InitializeComponent();
         }
 
-        float endresult;
-        float op;
-
-        enum State
-        {
-            Normal,
-            Addition,
-            Subtraction,
-            Multiplication,
-            Division
-        }
-
-        State state = State.Normal;
-
-        private void loadState(float number1, float number2, float endresult)
-        {
-            switch (state)
-            {
-                case State.Normal:
-                    
-
-                    break;
-                case State.Addition:
-
-                    endresult = number1 + number2;
-
-
-                    break;
-                case State.Subtraction:
-                    
-
-
-
-                    break;
-                case State.Multiplication:
-                    
-
-
-
-                    break;
-                case State.Division:
-
-
-                    break;
-            }
-        }
-
         private void textBox_zahl1_TextChanged(object sender, TextChangedEventArgs e)
         {
 
---
+++ Calculator/Common/calculate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    class calculate
+    {
+        float endresult;
+        float op;
+
+        enum State
+        {
+            Normal,
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
+        }
+
+        State state = State.Normal;
+
+        private void loadState(float number1, float number2, float endresult)
+        {
+            switch (state)
+            {
+                case State.Normal:
+                    
+
+                    break;
+                case State.Addition:
+
+                    endresult = number1 + number2;
+
+
+                    break;
+                case State.Subtraction:
+                    
+
+
+
+                    break;
+                case State.Multiplication:
+                    
+
+
+
+                    break;
+                case State.Division:
+
+
+                    break;
+            }
+        }
+    }
+}
